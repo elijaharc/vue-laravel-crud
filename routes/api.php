02701d32115696753c7ba77ecfef501bbe3faf7f
Elijah Jose Arcedera <elijaha@mosaic-solutions.com>
2022-03-16 23:57:35 +0800

    Fixes endpoints and methods

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,10 @@ Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
 Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::group(['prefix' => 'tasks', 'middleware' => 'auth:sanctum'], function () {
-    Route::get('/', [TaskController::class, 'index']);
-    Route::post('add', [TaskController::class, 'add']);
-    Route::get('edit/{id}', [TaskController::class, 'edit']);
-    Route::post('update/{id}', [TaskController::class, 'update']);
-    Route::delete('delete/{id}', [TaskController::class, 'delete']);
+Route::group(['prefix' => 'task', 'middleware' => 'auth:sanctum'], function () {
+    Route::get('/{userId}', [TaskController::class, 'index']);
+    Route::post('add', [TaskController::class, 'store']);
+    Route::get('edit/{taskId}', [TaskController::class, 'edit']);
+    Route::post('update', [TaskController::class, 'update']);
+    Route::delete('delete/{id}', [TaskController::class, 'destroy']);
 });
