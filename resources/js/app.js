d7f5b0fcdd70aa2eccd154f1c8888d438f31219c
Elijah Jose Arcedera <elijaha@mosaic-solutions.com>
2022-03-16 20:48:34 +0800

    Creates initial files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-require('./bootstrap');
+import { createApp } from 'vue'
+
+require('./bootstrap')
+import App from './App.vue'
+import axios from 'axios'
+import router from './router'
+import { plugin, defaultConfig } from '@formkit/vue'
+import '@formkit/themes/genesis'
+import BootstrapVue3 from "bootstrap-vue-3"
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css"
+
+const app = createApp(App)
+app.config.globalProperties.$axios = axios
+app.use(router)
+app.use(BootstrapVue3)
+app.use(plugin, defaultConfig)
+app.mount('#app')
+
+
---
+++ resources/js/router.js
@@ -0,0 +1,55 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+import Home from './pages/Home.vue';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
+import Tasks from './components/tasks/Task';
+import AddTask from './components/tasks/AddTask';
+import EditTask from './components/tasks/EditTask';
+
+export const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: Home
+    },
+    {
+        name: 'register',
+        path: '/register',
+        component: Register
+    },
+    {
+        name: 'login',
+        path: '/login',
+        component: Login
+    },
+    {
+        name: 'dashboard',
+        path: '/dashboard',
+        component: Dashboard
+    },
+    {
+        name: 'tasks',
+        path: '/tasks',
+        component: Tasks
+    },
+    {
+        name: 'addtask',
+        path: '/tasks/add',
+        component: AddTask
+    },
+    {
+        name: 'editask',
+        path: '/tasks/edit/:id',
+        component: EditTask
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+});
+
+export default router;
