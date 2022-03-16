b6b4e7da229d0d20d0e1b1e33910a2d4f5ede10c
Elijah Jose Arcedera <elijaha@mosaic-solutions.com>
2022-03-16 20:48:53 +0800

    Creates route files

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\TaskController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'tasks', 'middleware' => 'auth:sanctum'], function () {
+    Route::get('/', [TaskController::class, 'index']);
+    Route::post('add', [TaskController::class, 'add']);
+    Route::get('edit/{id}', [TaskController::class, 'edit']);
+    Route::post('update/{id}', [TaskController::class, 'update']);
+    Route::delete('delete/{id}', [TaskController::class, 'delete']);
 });
