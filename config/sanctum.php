b0a68e073a44755a3a613faf43e95fd7b61d60be
Elijah Jose Arcedera <elijaha@mosaic-solutions.com>
2022-03-17 09:17:39 +0800

    Adds heroku URL

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,localhost:8081,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,localhost:8081,https://elijah-vue-laravel-crud.herokuapp.com,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
