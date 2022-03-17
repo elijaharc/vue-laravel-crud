4ed8b7999e820df0ff0e280f3406fb92510b6345
Elijah Jose Arcedera <elijaha@mosaic-solutions.com>
2022-03-17 09:14:13 +0800

    Revert changes

--- config/database.php
+++ config/database.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-
-$url = parse_url(getenv("postgres://uwjasckfdpeise:e34f481070eb471bba3299e524714f6df578d37fb689848f72fa6069abe35859@ec2-52-44-209-165.compute-1.amazonaws.com:5432/dflndo32h86nob"));
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+$DATABASE_URL=parse_url('postgres://uwjasckfdpeise:e34f481070eb471bba3299e524714f6df578d37fb689848f72fa6069abe35859@ec2-52-44-209-165.compute-1.amazonaws.com:5432/dflndo32h86nob');
 
 return [
 
@@ -21,7 +16,7 @@ return [
     |
     */
 
-    'default' => 'pgsql',
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -69,16 +64,19 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => array(
-            'driver'   => 'pgsql',
-            'host'     => $host,
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
-            'charset'  => 'utf8',
-            'prefix'   => '',
-            'schema'   => 'public',
-        ),
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => $DATABASE_URL["DB_HOST"],
+            'port' => $DATABASE_URL["DB_PORT"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["DB_USER"],
+            'password' => $DATABASE_URL["DB_PASSWORD"],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
+        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
